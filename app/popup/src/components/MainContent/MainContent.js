c7be654da9196524425d93461ef07e9c1ef70313
Magalon <antoine@hakouna.com>
2018-07-19 13:50:59 -0400

    Basic Route

--- app/popup/src/components/MainContent/MainContent.js
+++ app/popup/src/components/MainContent/MainContent.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { MemoryRouter, Route, Switch } from 'react-router-dom';
 import './MainContent.css';
 
 import AccountView from './AccountView/AccountView.js';
+import Transaction from './AccountView/Transactions/Transactions';
 
 class MainContent extends Component {
     render() {
         return (
-            <div className="mainContent">
-                <AccountView />
-            </div>
+            <MemoryRouter className="mainContent">
+                <Switch>
+                    <Route path="/" exact component={AccountView} />
+                    <Route path="/page1" exact component={Transaction} />
+                </Switch>
+            </MemoryRouter>
         );
     }
 }
