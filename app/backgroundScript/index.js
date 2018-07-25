cc4209657523b86fc73d0fa98620f5cd87d439dc
kondax <kondaxsolutions@gmail.com>
2018-07-25 23:34:17 +0100

    Fixes #52 - Refactors backgroundScript

--- app/backgroundScript/index.js
+++ app/backgroundScript/index.js
@@ -1,168 +1,187 @@
+// Libraries
 import PortHost from 'lib/communication/PortHost';
 import PopupClient from 'lib/communication/popup/PopupClient';
 import LinkedResponse from 'lib/messages/LinkedResponse';
 import Wallet from './wallet';
 import Logger from 'lib/logger';
 import TronUtils from 'TronUtils';
+import randomUUID from 'uuid/v4';
 
-const rpc = new TronUtils.rpc();
+// Constants
+import { CONFIRMATION_TYPE } from 'lib/constants';
 
+// Initialise utilities
 const logger = new Logger('backgroundScript');
-import tron from 'TronUtils';
 const portHost = new PortHost();
 const popup = new PopupClient(portHost);
 const linkedResponse = new LinkedResponse(portHost);
 const wallet = new Wallet();
 
-const CONFIRMATION_TYPE = {
-    SEND : "SEND"
-};
-
-const pendingConfirmations = {};
-
 logger.info('Script loaded');
 
-let currentConfirmationId = 0;
-let popup2 = null;
+const pendingConfirmations = {};
+let dialog = false;
 
-function addConfirmation(confirmation, resolve, reject){
-    logger.info('Adding confirmation: ');
-    logger.info(confirmation);
+function addConfirmation(confirmation, resolve, reject) {
+    confirmation.id = randomUUID();
+    logger.info('Adding confirmation:', confirmation);
 
-    currentConfirmationId++;
-    confirmation.id = currentConfirmationId;
     pendingConfirmations[confirmation.id] = {
         confirmation,
         resolve,
         reject
     };
 
-
     popup.sendNewConfirmation(confirmation);
-    if(popup2){
-        popup2.focus();
-    }else{
-        popup2 = window.open("app/popup/build/index.html", "extension_popup", "width=420,height=595,status=no,scrollbars=yes,resizable=false");
-    }
+
+    if (dialog)
+        dialog.focus();
+    else dialog = window.open('app/popup/build/index.html', 'extension_popup', 'width=420,height=595,status=no,scrollbars=yes,resizable=false');
 }
 
-function getConfirmations(){
-    let out = [];
-    let keys = Object.keys(pendingConfirmations);
-    for(let i in keys){
-        out.push(pendingConfirmations[keys[i]].confirmation);
-    }
-    return out;
-}
-//open popup
+function closeDialog() {
+    if(Object.keys(pendingConfirmations).length)
+        return;
 
-function closePopup2IfQueueEmpty(){
-    if(Object.keys(pendingConfirmations) <= 0 && popup2){
-        popup2.close();
-        popup2 = null;
-    }
+    if(!dialog)
+        return;
+
+    dialog.close();
+    dialog = false;
 }
 
-popup.on('declineConfirmation', ({data, resolve, reject})=>{
-    if(!pendingConfirmations[data.id])
-        alert("tried denying confirmation, but confirmation went missing.");
-    pendingConfirmations[data.id].resolve("denied");
-    delete pendingConfirmations[data.id];
-    resolve();
-    closePopup2IfQueueEmpty();
-});
+popup.on('declineConfirmation', ({
+    data,
+    resolve,
+    reject
+}) => {
+    const { id: confirmationID } = data;
 
-popup.on('acceptConfirmation', ({data, resolve, reject})=>{
-    if(!pendingConfirmations[data.id])
-        alert("tried accepting confirmation, but confirmation went missing.");
+    if (!pendingConfirmations.hasOwnProperty(confirmationID))
+        return logger.warn(`Attempted to reject non-existent confirmation ${confirmationID}`);
 
-    let confirmation = pendingConfirmations[data.id];
+    const confirmation = pendingConfirmations[confirmationID];
 
-    logger.info('accepting confirmation');
+    logger.info(`Declining confirmation ${confirmationID}`);
     logger.info(confirmation);
-    confirmation.resolve("accepted " + JSON.stringify(confirmation.confirmation));
 
+    confirmation.reject('denied');
     delete pendingConfirmations[data.id];
+    
+    closeDialog();
     resolve();
-    closePopup2IfQueueEmpty();
 });
 
-popup.on('getConfirmations', ({data, resolve, reject})=>{
-    logger.info('getConfirmations called');
-    resolve(getConfirmations());
+popup.on('acceptConfirmation', ({
+    data,
+    resolve,
+    reject
+}) => {
+    const { id: confirmationID } = data;
+
+    if (!pendingConfirmations.hasOwnProperty(confirmationID))
+        return logger.warn(`Attempted to resolve non-existent confirmation ${confirmationID}`);
+
+    const confirmation = pendingConfirmations[confirmationID];
+
+    logger.info(`Accepting confirmation ${confirmationID}`);
+    logger.info(confirmation);
+
+    confirmation.resolve(`accepted ${JSON.stringify(confirmation.confirmation)}`);
+    delete pendingConfirmations[data.id];
+    
+    closeDialog();
+    resolve();
 });
 
-popup.on('setPassword', ({data, resolve, reject})=>{
-    logger.info('before, wallet:');
-    logger.info(wallet);
-    if(wallet.isInitialized()){
-        alert("Wallet already initialized. Need to explicitly clear before doing this.");
-    }else{
-        wallet.initWallet(data.password);
-    }
+popup.on('getConfirmations', ({
+    data,
+    resolve,
+    reject
+}) => {
+    logger.info('Requesting confirmation list');
+
+    const confirmations = Object.values(pendingConfirmations).map(({ confirmation }) => {
+        return confirmation
+    });
+
+    resolve(confirmations);
 });
 
-async function updateAccount(){
+popup.on('setPassword', ({
+    data,
+    resolve,
+    reject
+}) => {
+    logger.info('Setting password for wallet', { wallet });
+
+    if (wallet.isSetup())
+        return logger.warn('Attempted to set password post initialisation');
+
+    wallet.setupWallet(data.password);
+});
+
+async function updateAccount() {
+    logger.info('Requesting account update');
+
     await wallet.updateAccounts();
 
-    let account = wallet.getAccount();
-    popup.sendAccount(account);
+    popup.sendAccount(
+        wallet.getAccount()
+    );
 }
 
-popup.on('unlockWallet', ({data, resolve, reject})=>{
-    logger.info('unlockWallet');
-    logger.info(data);
-    resolve(wallet.unlockWallet(data.password));
-    updateAccount();
+popup.on('unlockWallet', ({
+    data,
+    resolve,
+    reject
+}) => {
+    logger.info('Requesting to unlock wallet');
+
+    const success = wallet.unlockWallet(data.password);
+
+    if(success)
+        updateAccount();
+
+    resolve(success);
 });
 
-popup.on('getWalletStatus', ({data, resolve, reject})=>{
-    resolve(wallet.getStatus());
+popup.on('getWalletStatus', ({ data, resolve, reject }) => {
+    logger.info('Requesting wallet status');
+    resolve(wallet.status);
 });
 
-const handleWebCall = ({ request: { method, args = {} }, resolve, reject }) => {
-    switch(method) {
+const handleWebCall = ({
+    request: {
+        method,
+        args = {}
+    },
+    resolve,
+    reject
+}) => {
+    switch (method) {
         case 'sendTron':
             addConfirmation({
-                type : CONFIRMATION_TYPE.SEND,
-                from : args.from,
-                amount : args.amount
-            }, resolve, reject);
-        break;
-        case 'signTransaction':
-            // Expects { signedTransaction: string, broadcasted: bool, transactionID: string }
-
-            const { 
-                transaction,
-                broadcast
-            } = args;
-            
-            resolve({
-                signedTransaction: btoa(String(transaction)),
-                broadcasted: false,
-                transaction: false
-            });
-        break;
-        case 'getTransaction':
-            // Expects { transaction: obj }
-            reject('Method pending implementation');
-        break;
-        case 'getUserAccount':
-            // Expects { address: string, balance: integer }
-            reject('Method pending implementation');
-        break;
-        case 'simulateSmartContract':
-            // I'm not sure what the input / output will be here
-            reject('Method pending implementation');
-        break;
+                type: CONFIRMATION_TYPE.SEND_TRON,
+                amount: args.amount
+            }, resolve, reject);        
         default:
             reject('Unknown method called (' + method + ')');
     }
 };
 
-linkedResponse.on('request', ({ request, resolve, reject }) => {
-    if(request.method)
-        return handleWebCall({ request, resolve, reject });
+linkedResponse.on('request', ({
+    request,
+    resolve,
+    reject
+}) => {
+    if (request.method) {
+        return handleWebCall({
+            request,
+            resolve,
+            reject
+        });
+    }
 
-    // other functionality here or w/e
+    reject('Unknown protocol called');
 });
