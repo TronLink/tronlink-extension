840408e4b5a9ef9869c1a0281f3df011f6aee690
metjm <metjm.mail@gmail.com>
2018-07-29 08:05:18 +0200

    connect testnet coin faucet

--- app/popup/src/components/Main/Content/Give/index.js
+++ app/popup/src/components/Main/Content/Give/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
 import './Give.css';
+import axios from 'axios';
 
 class Give extends Component {
     constructor(props) {
@@ -12,20 +13,29 @@ class Give extends Component {
         };
     }
 
+    async fund(){
+        axios.get('https://us-central1-flottpay.cloudfunctions.net/testCoins?address=' + this.props.account.address).then(x => x.data);
+        alert("Sent 20k testnet coins. They can take a minute to show up.");
+        this.props.history.push('/main/transactions');
+    }
+
     render() {
         return (
             <div className="give">
                 <div className="giveContainer">
                     <div className="giveHeader">TestNet Funding :</div>
-                    <div className="giveSubHeader">Sends 20,000 TRX to your currently selected wallet address.</div>
+                    <div className="giveSubHeader">Sends 20,000 Testnet TRX to your currently selected wallet address.</div>
                     <div className="giveSubHeader">Address: { this.props.account.address }</div>
                 </div>
-                <div className="giveBtn button black">Fund Account</div>
+                <div className="giveBtn button black" onClick={this.fund.bind(this)}>Fund Account</div>
             </div>
         );
     }
 }
 
-export default connect(state => ({
-    account: state.wallet.account
-}))(Give);
+
+export default withRouter(
+    connect(
+        state => ({ account : state.wallet.account }),
+    )(Give)
+);
