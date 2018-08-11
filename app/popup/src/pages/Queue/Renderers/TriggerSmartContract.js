3b4293c9329ccdb3f40db4c0518cd4045a364925
kondax <kondaxsolutions@gmail.com>
2018-08-11 13:17:00 +0100

    Add localisation to triggerSmartContract confirmation

--- app/popup/src/pages/Queue/Renderers/TriggerSmartContract.js
+++ app/popup/src/pages/Queue/Renderers/TriggerSmartContract.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 export default class TriggerSmartContract extends Component {
     render() {
-        //const confirmation = this.props.confirmation;
         return (
             <div className="confirmSend">
-                {this.props.queueLength}
+                { this.props.queueLength  }
 
-                <div className="confirmSmartContract bold">Trigger Smart Contract</div>
+                <div className="confirmSmartContract bold">
+                    <FormattedMessage id='queue.smartContract.trigger' />
+                </div>
+                
                 <div className="confirmGroup">
-                    <textarea
-                        value=""
-                        className="confirmTextArea"
-                        readonly/>
+                    <textarea value="" className="confirmTextArea" readonly/>
                 </div>
 
-                {this.props.buttons}
+                { this.props.buttons }
             </div>
         );
     }
